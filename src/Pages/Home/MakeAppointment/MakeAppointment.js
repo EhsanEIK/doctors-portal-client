d4393aee13d7b283f136473bb39e59fae20c6c38
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-12 17:16:28 +0600

    added MakeAppointment section in Home page

---
+++ src/Pages/Home/MakeAppointment/MakeAppointment.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import doctor from '../../../assets/images/doctor-small.png';
+import appointment from '../../../assets/images/appointment.png';
+
+const MakeAppointment = () => {
+    return (
+        <section className='mt-32' style={{ backgroundImage: `url(${appointment})`, backgroundPosition: "center", backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
+            <div className="hero">
+                <div className="hero-content text-base-100 flex-col lg:flex-row md:pb-0 p-5">
+                    <img src={doctor} alt="" className="hidden md:block w-1/2 -mt-32" />
+                    <div>
+                        <h1 className="md:text-5xl text-3xl font-bold">Box Office News!</h1>
+                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default MakeAppointment;
