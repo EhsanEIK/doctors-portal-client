23962be3f9778208e8e95316ca853a369b8476c0
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-12 10:32:12 +0600

    added max-width in App.js file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import router from './Routes/Routes/routes';
 
 function App() {
   return (
-    <div>
+    <div className='max-w-[1440px] mx-auto'>
       <RouterProvider router={router}></RouterProvider>
     </div>
   );
