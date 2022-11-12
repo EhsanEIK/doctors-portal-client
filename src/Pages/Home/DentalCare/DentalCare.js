e8cc9fa84b0978131dfe23c5ffe4c416a5499a4c
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-12 17:03:59 +0600

    added DentalCare section in Home page

---
+++ src/Pages/Home/DentalCare/DentalCare.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import treatment from '../../../assets/images/treatment.png'
+
+const DentalCare = () => {
+    return (
+        <section>
+            <div className="hero bg-base-100 mt-32 md:p-28">
+                <div className="hero-content flex-col lg:flex-row">
+                    <img src={treatment} alt="" className="rounded-lg shadow-2xl" style={{ width: "458px", height: "470px" }} />
+                    <div className='md:ml-28'>
+                        <h1 className="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
+                        <p className="py-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page.</p>
+                        <button className="btn btn-primary text-base-100">Get Started</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default DentalCare;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import DentalCare from '../DentalCare/DentalCare';
 import InfoCards from '../InfoCards/InfoCards';
 import Services from '../Services/Services';
 
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <InfoCards></InfoCards>
             <Services></Services>
+            <DentalCare></DentalCare>
         </div>
     );
 };
