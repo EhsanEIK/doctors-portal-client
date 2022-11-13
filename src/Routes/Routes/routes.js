215674922fea374c06a8354ab930d3ef6110d21f
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-13 09:56:34 +0600

    added Appointment route and page

--- src/Routes/Routes/routes.js
+++ src/Routes/Routes/routes.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layouts/Main";
+import Appointment from "../../Pages/Appointment/Appointment";
 import Home from "../../Pages/Home/Home/Home";
 
 const router = createBrowserRouter([
@@ -7,7 +8,8 @@ const router = createBrowserRouter([
         path: '/',
         element: <Main></Main>,
         children: [
-            { path: '/', element: <Home></Home> }
+            { path: '/', element: <Home></Home> },
+            { path: '/appointment', element: <Appointment></Appointment> },
         ]
     }
 ])
