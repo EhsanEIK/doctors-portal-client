ae43ab8d93f1f8098784048f49ef61d11a7b566c
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-14 21:57:36 +0600

    added bg image in AppointmentTopBanner

--- src/Pages/Appointment/AppointmentTopBanner/AppointmentTopBanner.js
+++ src/Pages/Appointment/AppointmentTopBanner/AppointmentTopBanner.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { DayPicker } from 'react-day-picker';
 import chair from '../../../assets/images/chair.png';
+import bg from '../../../assets/images/bg.png';
 
 const AppointmentTopBanner = ({ selectedDate, setSelectedDate }) => {
 
     return (
         <section>
-            <div className="hero bg-base-100 md:py-28 py-10">
+            <div className="hero bg-base-100 md:py-28 py-10" style={{ backgroundImage: `url(${bg})`, backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat" }}>
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <img src={chair} alt="" className="md:w-1/2 w-full rounded-lg shadow-2xl" />
                     <div className='lg:mr-28 lg:mt-0 mt-10'>
