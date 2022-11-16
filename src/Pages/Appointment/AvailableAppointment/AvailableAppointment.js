e664c4aeaf320500ffc2b79a4f929c29d6d6bf37
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-16 17:25:24 +0600

    added Loading component and added isLoading during fetching the appointmentOptions data

--- src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { format } from 'date-fns';
 import React, { useState } from 'react';
+import Loading from '../../Shared/Loading/Loading';
 import BookingModal from '../BookingModal/BookingModal';
 import AppointmentOption from './AppointmentOption';
 
@@ -8,11 +9,15 @@ const AvailableAppointment = ({ selectedDate }) => {
     const [treatment, setTreatment] = useState([]);
     const date = format(selectedDate, "PP");
 
-    const { data: appointmentOptions = [], refetch } = useQuery({
+    const { data: appointmentOptions = [], refetch, isLoading } = useQuery({
         queryKey: ['appointmentOptions', date],
         queryFn: () => fetch(`http://localhost:5000/appointmentOptions?date=${date}`)
             .then(res => res.json())
-    })
+    });
+
+    if (isLoading) {
+        <Loading></Loading>
+    }
 
     return (
         <section className='my-32'>
