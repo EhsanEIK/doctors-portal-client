7ba7cb9db2e186ca8f02fed68185b8170f23bb3e
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-17 19:17:03 +0600

    added useAdmin custom hook and get the admin boolean value from database

--- src/layouts/DashboardLayout.js
+++ src/layouts/DashboardLayout.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthProvider/AuthProvider';
+import useAdmin from '../hooks/useAdmin';
 import Navbar from '../Pages/Shared/Navbar/Navbar';
 
 const DashboardLayout = () => {
+    const { user } = useContext(AuthContext);
+    const [isAdmin] = useAdmin(user?.email);
+
     return (
         <div>
             <Navbar></Navbar>
@@ -14,8 +19,11 @@ const DashboardLayout = () => {
                 <div className="drawer-side">
                     <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+                        {user && <h3>Welcome {user?.email}</h3>}
                         <li><Link to='/dashboard'>My Appointments</Link></li>
-                        <li><Link to='/dashboard/users'>All Users</Link></li>
+                        {
+                            isAdmin && <li><Link to='/dashboard/users'>All Users</Link></li>
+                        }
                     </ul>
                 </div>
             </div>
