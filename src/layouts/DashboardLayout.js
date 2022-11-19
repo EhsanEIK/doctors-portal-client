6a27c7c2a8f47306693ee675739f432f8fe997a9
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-19 17:00:47 +0600

    fixed a bug in DashboardLayout.js file

--- src/layouts/DashboardLayout.js
+++ src/layouts/DashboardLayout.js
@@ -18,7 +18,7 @@ const DashboardLayout = () => {
                 </div>
                 <div className="drawer-side">
                     <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
-                    <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+                    <ul className="menu p-4 w-80 text-base-content">
                         {user && <h3>Welcome {user?.email}</h3>}
                         <li><Link to='/dashboard'>My Appointments</Link></li>
                         {
