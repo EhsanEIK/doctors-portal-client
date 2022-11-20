cb99d5dd69dfda16a620e40ceadb36ac34b2d565
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-20 11:12:26 +0600

    loaded booking data in Payment page and installed stripe

--- src/Pages/Appointment/Payment/Payment.js
+++ src/Pages/Appointment/Payment/Payment.js
@@ -3,10 +3,12 @@ import { useLoaderData } from 'react-router-dom';
 
 const Payment = () => {
     const booking = useLoaderData();
+    const { treatment, price, appointmentDate, slot } = booking;
 
     return (
         <div>
-            <h1 className='text-3xl font-semibold mb-5'>Payment</h1>
+            <h1 className='text-3xl font-semibold mb-2'>Payment for {treatment}</h1>
+            <p className='text-lg'>Please pay <strong>${price}</strong> for your appointment on {appointmentDate} at {slot}</p>
         </div>
     );
 };
