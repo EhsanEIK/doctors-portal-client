0f6dbdb1acd3f3c33b6a82a18589aef27466f94c
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-20 09:07:32 +0600

    displayed price in AppointmentOption

--- src/Pages/Appointment/AvailableAppointment/AppointmentOption.js
+++ src/Pages/Appointment/AvailableAppointment/AppointmentOption.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const AppointmentOption = ({ appointmentOption, setTreatment }) => {
-    const { name, slots } = appointmentOption;
+    const { name, slots, price } = appointmentOption;
 
     return (
         <div className="card md:w-96 w-full bg-base-100 shadow-xl md:mt-0 mt-5">
@@ -9,6 +9,7 @@ const AppointmentOption = ({ appointmentOption, setTreatment }) => {
                 <h2 className="text-xl font-bold text-primary text-center">{name}</h2>
                 <p className='text-center'>{slots.length ? slots[0] : 'Try another day'}</p>
                 <p className='text-center'>{slots.length && slots.length > 1 ? `${slots.length} spaces available` : `${slots.length} space available`}</p>
+                <p className='text-center'>Price: ${price}</p>
                 <div className="card-actions justify-center">
                     <label
                         disabled={slots.length === 0}
